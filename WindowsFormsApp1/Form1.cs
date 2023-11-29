9348522f35b6c0f252327b5ba8cb001aa6f761f5
Batuhan81 <celebibatuhan4@gmail.com>
2023-11-29 10:40:16 +0300

    Metin Kutusu Çalışmaları
    C# Visiul Studiyou kullanarak metin kutusuna sadece sayı ,sadece harf girilmesini sağlamak ve metin kutusuna girilen ifadenin ilk harfini büyük yazar ve bunların hepsini bir labelda gösterir

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(char.IsNumber(e.KeyChar)|| e.KeyChar =='.'||(sender as TextBox).Text.IndexOf(',')>-1)
+            {
+                e.Handled = true;
+            }
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sınıf.sa=textBox1.Text;
+            label1.Text=sınıf.sa;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) || e.KeyChar == '.' || (sender as TextBox).Text.IndexOf(',') > -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sınıf.A = Convert.ToInt32(textBox2.Text);
+                label1.Text = sınıf.A.ToString();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            int a= Convert.ToInt32( textBox2.Text );    
+            textBox2.Text = a.ToString("#,###.00");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sınıf.Büyükyaz=textBox3.Text;
+            label1.Text=sınıf.Büyükyaz.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            sınıf.F = textBox3.Text;
+            label1.Text=sınıf.F.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/sınıf.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public static class sınıf
+    {
+        private static string SA;
+        public static string sa
+        {
+            get { return SA; }
+            set { SA = value; }
+        }
+        private static int a;
+        public static int A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        private static string büyükyaz;
+        public static string Büyükyaz
+        {
+            get
+            {
+                return büyükyaz;
+            }
+            set
+            {
+                büyükyaz = value.Substring(0, 1).ToUpper()+value.Substring(1).ToLower();
+            }
+        }
+
+
+
+
+
+
+        private static string f;
+        public static string F
+        {
+            get
+            {
+                return f;
+            }
+            set
+            {
+                f= value;
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
